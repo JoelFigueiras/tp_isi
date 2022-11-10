86007c6b055aec727bd4c3e9cd9d0ee62e3cbe8b
Joel Figueiras <joelfigueiras@live.com.pt>
2022-11-10 17:22:30 +0000

    tp_isi

---
+++ carros/jobs/transformacoes/inputs/c#/project_isi/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace project_isi
+{
+    internal class Program
+    {
+        // ^[A-Z][a-z]* ([A-Z]|[0-9])([a-z]|[0-9]).* (expressao regular para marcas de carros)
+        // ([1]|[2])[0-9]{3} (expressão regular para ano dos carros)
+        // [0-9]* (preço carro)
+        // [0-9]* (km's carro)
+        //[A-Z](([a-z]|[A-Z])+) (combustivel)
+        // [A-Z]([a-z]+) (tipo de caixa velocidades)
+        //[A-Z]([a-z]+) (numero propietario)
+        //@"^[A-Z][a-z]* ([A-Z]|[0-9])([a-z]|[0-9]).*,([1]|[2])[0-9]{3},[0-9]*,[0-9]*,[A-Z](([a-z]|[A-Z])+),[A-Z]([a-z]+),[A-Z]([a-z]+))$";
+        static void Main(string[] args)
+        {
+
+            string ficheiro = @"..\..\csv_files\car_details.csv";
+            string destinojson = @"..\..\json_files\car_details.json";            
+            string destinoxml = @"..\..\xml_files\car_details.xml";
+            string er = @"[^,]+,([1]|[2])[0-9]{3},[0-9]*,[0-9]*,[A-Za-z,]+,[^\n]+";
+            string csvString = null;
+            List<Veiculo> listaVeiculos = new List<Veiculo>();
+            string jsonString;
+
+
+            foreach (string line in File.ReadLines(ficheiro))
+            {
+                csvString = line;
+                Regex g = new Regex(er);
+                Match m = g.Match(csvString);
+
+                if (m.Success)
+                {
+
+                    string[] campos = m.Value.Split(',');
+                    string nome = campos[0];
+                    int ano = Convert.ToInt32(campos[1]);
+                    int preco = Convert.ToInt32(campos[2]);
+                    int km = Convert.ToInt32(campos[3]);
+                    string fuel = campos[4];
+                    string transmissao = campos[6];
+                    string n_dono = campos[7];
+                    string tipo_dono = campos[5];
+                    Veiculo veiculo = new Veiculo(nome, ano, preco, km, fuel, transmissao, n_dono, tipo_dono);
+
+                    listaVeiculos.Add(veiculo);
+
+                }
+                else
+                {
+
+
+                    Console.WriteLine($"{line}");
+                }
+
+
+            }
+         
+            jsonString = System.Text.Json.JsonSerializer.Serialize(listaVeiculos);
+            jsonString = "{\"Carro\": " + jsonString + "}";
+
+
+            if (!File.Exists(destinojson))
+            {
+                File.WriteAllText(destinojson, jsonString);
+            }
+            else
+            {
+                File.WriteAllText(destinojson, jsonString);
+            }
+            XNode node = JsonConvert.DeserializeXNode(jsonString, "Carros");
+            XmlDocument doc = new XmlDocument();
+            File.WriteAllText(destinoxml, node.ToString());
+
+        }
+    }
+}
---
+++ carros/jobs/transformacoes/inputs/c#/project_isi/Veiculo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isi
+{
+    internal class Veiculo
+    {
+        string nome;
+        int ano;
+        int preco;
+        int km;
+        string fuel;
+        string transmissao;
+        string n_dono;
+        string tipo_dono;
+
+        #region Construtor
+        public Veiculo(string nome, int ano, int preco, int km, string fuel, string transmissao, string n_dono, string tipo_dono)
+        {
+            this.nome = nome;
+            this.ano = ano;
+            this.preco = preco;
+            this.km = km;
+            this.fuel = fuel;
+            this.transmissao = transmissao;
+            this.n_dono = n_dono;
+            this.tipo_dono = tipo_dono;
+        }
+        #endregion
+
+        #region Properties
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+        public int Preco
+        {
+            get { return preco; }
+            set
+            {
+                preco = value;
+            }
+        }
+        public int Km
+        {
+            get { return km; }
+            set
+            {
+                km = value;
+            }
+        }
+        public string Fuel
+        {
+            get { return fuel; }
+            set
+            {
+                fuel = value;
+            }
+        }
+        public string Transmissao
+        {
+            get { return transmissao; }
+            set
+            {
+                transmissao = value;
+            }
+        }
+        public string N_dono
+        {
+            get { return n_dono; }
+
+            set
+            {
+                n_dono = value;
+            }
+        }
+
+        public string Tipo_dono
+        {
+            get { return tipo_dono; }
+            set
+            {
+                tipo_dono = value;
+            }
+        }
+        #endregion
+
+
+    }
+}
